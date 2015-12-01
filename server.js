63abbbc9bfa1b443276c58e86fa353713bf29514
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-01 17:15:54 -0500

    Add npm packages; express, body-parser, morgan and request

---
+++ server.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const express     = require('express');
+const app         = express();
+const bodyParser  = require('body-parser');
+const logger      = require('morgan');
+const request     = require('request');
