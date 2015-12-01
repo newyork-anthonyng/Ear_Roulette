30070323b6d65d667d2a94b3dd563740cc2fd6d1
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-01 18:08:58 -0500

    Add route /albums/:artistID, which returns an array of album objects. It is currently using a test Spotify link

--- server.js
+++ server.js
@@ -14,6 +14,49 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// TODO: need to populate mongodb with list of artists
+// get a list of artists from database
+app.get('/artists', (req, res) => {
+  let myArtists = ['matchbox twenty', 'killers'];
+
+  res.send(myArtists);
+});
+
+// returns an array of Album objects
+app.get('/albums/:artistID', (req, res) => {
+  let artistID = req.params.artistID;
+  // let myUrl = 'https://api.spotify.com/v1/artists/' +
+  //           artistID + '/albums?limit=10';
+
+  // test URL
+  let myUrl = 'https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?limit=5';
+
+  // hit spotify API
+  request(myUrl, (error, response, body) => {
+    if(!error && response.statusCode == 200) {
+
+      // jsonData will hold an array of albums
+      let jsonData = JSON.parse(body)['items'];
+
+      // myAlbums will hold an array of Album objects
+      // Album object will hold 'id', 'name' and 'image'
+      let myAlbums = [];
+
+      for(let i = 0, j = jsonData.length; i < j; i++) {
+        let newAlbum = {};
+
+        newAlbum['id']    = jsonData[i]['id'];
+        newAlbum['name']  = jsonData[i]['name'];
+        newAlbum['image'] = jsonData[i]['images'][1]['url'];
+
+        myAlbums.push(newAlbum);
+      }
+
+      res.send(myAlbums);
+    }
+  });
+});
+
 const server = app.listen(3000, () => {
   console.log('Express server running...');
 })
