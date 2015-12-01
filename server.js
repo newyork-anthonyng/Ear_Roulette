a53bcf26633fe24bbcf0dfcf355fbb3d13d08e3d
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-01 17:19:48 -0500

    Set up bodyParser and server

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const app         = express();
 const bodyParser  = require('body-parser');
 const logger      = require('morgan');
 const request     = require('request');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const server = app.listen(3000, () => {
+  console.log('Express server running...');
+})
