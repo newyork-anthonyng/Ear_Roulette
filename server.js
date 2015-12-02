ab3cb1d8cbf2aecfa69258c92b63d257cd13ed21
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-01 19:05:07 -0500

    Add /tracks/:albumID route; this route will return an array of tracks from an album

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/artists', (req, res) => {
   res.send(myArtists);
 });
 
-// return an object with an 'id' (which can be searched on Spotify) and 'name'
+// returns an object with an 'id' (which can be searched on Spotify) and 'name'
 app.get('/artistID/:artistName', (req, res) => {
   let artistName    = req.params.artistName;
   let formattedName = artistName.split(' ').join('+');
@@ -52,7 +52,7 @@ app.get('/artistID/:artistName', (req, res) => {
   });
 });
 
-// returns an array of Album objects
+// returns an array of objects, with keys of 'id', 'name' and 'image'
 app.get('/albums/:artistID', (req, res) => {
   let artistID = req.params.artistID;
   // let myUrl = 'https://api.spotify.com/v1/artists/' +
@@ -68,8 +68,8 @@ app.get('/albums/:artistID', (req, res) => {
       // jsonData will hold an array of albums
       let jsonData = JSON.parse(body)['items'];
 
-      // myAlbums will hold an array of Album objects
-      // Album object will hold 'id', 'name' and 'image'
+      // myAlbums will hold an array of objects
+      // Object will hold 'id', 'name' and 'image'
       let myAlbums = [];
 
       for(let i = 0, j = jsonData.length; i < j; i++) {
@@ -87,7 +87,38 @@ app.get('/albums/:artistID', (req, res) => {
   });
 });
 
-// TODO get tracks from album
+// returns an array of objects, with keys of 'id', 'title', 'artist' & 'preview'
+app.get('/tracks/:albumID', (req, res) => {
+  let albumID = req.params.albumID;
+
+  // TODO get real URL to work
+  // let myURL = 'https://api.spotify.com/v1/albums/' + albumID + '/tracks?limit=30';
+  let myURL = 'https://api.spotify.com/v1/albums/2nrtgcTaTEy4KF742WvVC3/tracks?limit=30';
+
+  request(myURL, (error, response, body) => {
+    if(!error && response.statusCode == 200) {
+      let jsonData = JSON.parse(body)['items'];
+
+      // myTracks will hold an array of objects
+      // Object will hold 'id', 'title', 'artist' and 'preview'
+      let myTracks = [];
+
+      for(let i = 0, j = jsonData.length; i < j; i++) {
+        let newTrack = {};
+
+        newTrack['id']      = jsonData[i]['id'];
+        newTrack['title']   = jsonData[i]['name'];
+        newTrack['artist']  = jsonData[i]['artists'][0]['name'];
+        newTrack['preview'] = jsonData[i]['preview_url'];
+
+        myTracks.push(newTrack);
+      }
+
+      res.send(myTracks);
+    }
+  });
+
+});
 
 
 const server = app.listen(3000, () => {
