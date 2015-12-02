ca5630baf2bf22d88e48727c6a290136dcdcfc03
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-01 20:05:50 -0500

    Add function to recursively retrieve all artists Id

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,32 @@
+'use strict'
+
+let myArtists = ['billy joel', 'cher', 'madonna'];
+
 $(function() {
-  console.log('inside of script.js');
+  $('#artist').click(() => {
+    console.log('button clicked');
+    // let i = 0;
+    // getArtistID(i);
+    getArtistId(myArtists);
+  });
 });
+
+let getArtistId = function(artistArray) {
+
+  $.ajax({
+    url: '/artistID/' + artistArray[0]
+  }).done((artist) => {
+    console.log(artist['name'] + ': ' + artist['id']);
+  });
+
+  // make a copy of artist array
+  // remove the first artist from the array and call getArtistId again
+  let myArtistArray = artistArray;
+  myArtistArray.splice(0, 1);
+
+  if(myArtistArray.length > 0) {
+    getArtistId(myArtistArray);
+  } else {
+    return true;
+  }
+}
