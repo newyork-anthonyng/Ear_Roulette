120645981b78c5556f980e51d05c6bdedfc73c28
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-02 09:35:58 -0500

    Add $.when function to get all Albums

--- public/js/script.js
+++ public/js/script.js
@@ -4,18 +4,30 @@ let myArtists = ['billy joel', 'cher', 'madonna'];
 
 $(function() {
   $('#artist').click(() => {
-    let myArtistId = [];
-    let deferreds = getArtistIdPromises(myArtists, myArtistId);
+    let myArtistIdArray = [];
+    let myAlbumIdArray  = [];
 
-    $.when.apply($, deferreds).done(function(data) {
-      console.log(myArtistId);
-    });
-  });
+    let deferredArtistId = getArtistIdPromises(myArtists, myArtistIdArray);
+
+    $.when.apply($, deferredArtistId).done(() => {
+      // When all Artist Id's are stored, get each album from the artist
+      // ...and store into myAlbumIdArray
+      console.log('All artist ID\'s retrieved');
+      console.log(myArtistIdArray);
+      let deferredAlbumId = getAlbumsPromises(myArtistIdArray, myAlbumIdArray);
+
+      $.when.apply($, deferredAlbumId).done(() => {
+        console.log('All album ID\'s retrieved');
+        console.log(myAlbumIdArray);
+
+      }); // close $.when for getting Album ID's
+    }); // close $.when for getting Artist ID's
+  }); // close $('artist').click event
 
 });
 
 // 'artistArray' holds an array of artist names
-// 'compiledArray' is the array which will hold all of your artist data
+// 'compiledArray' will hold all returned Artist ID's
 // method will return an array of AJAX calls
 let getArtistIdPromises = function(artistArray, compiledArray) {
   let deferreds = [];
@@ -24,7 +36,31 @@ let getArtistIdPromises = function(artistArray, compiledArray) {
     let newRequest = $.ajax({
       url: 'artistID/' + artistArray[i]
     }).done((data) => {
-      compiledArray.push(data);
+      compiledArray.push(data['id']);
+    });
+
+    deferreds.push(newRequest);
+  }
+
+  return deferreds;
+}
+
+// 'artistIDArray' holds an array of artist ID's
+// 'compiledArray' will hold all returned Album ID's
+// method will return an array of AJAX calls
+let getAlbumsPromises = function(artistIDArray, compiledArray) {
+  console.log('getting album promises');
+  let deferreds = [];
+
+  for(let i = 0, j = artistIDArray.length; i < j; i++) {
+    let newRequest = $.ajax({
+      url: 'albums/' + artistIDArray[i]
+    }).done((data) => {
+      // data returns an array of Album objects
+      for(let i = 0, j = data.length; i < j; i++) {
+        compiledArray.push(data[i]['id']);
+      }
+
     });
 
     deferreds.push(newRequest);
