c2a7e252e76d86a662bf856810b47267c6e4afd6
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-02 10:04:00 -0500

    Add $.when to retrieve all track information

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,33 @@
 'use strict'
 
-let myArtists = ['billy joel', 'cher', 'madonna'];
+let myArtists = ['billy joel', 'cher', 'madonna', 'killers'];
 
 $(function() {
   $('#artist').click(() => {
     let myArtistIdArray = [];
     let myAlbumIdArray  = [];
+    let myTracksArray   = [];
 
     let deferredArtistId = getArtistIdPromises(myArtists, myArtistIdArray);
 
+    // When all Artist Id's are stored, get each album from the artist
     $.when.apply($, deferredArtistId).done(() => {
-      // When all Artist Id's are stored, get each album from the artist
-      // ...and store into myAlbumIdArray
       console.log('All artist ID\'s retrieved');
-      console.log(myArtistIdArray);
+
       let deferredAlbumId = getAlbumsPromises(myArtistIdArray, myAlbumIdArray);
 
+      // When all Album Id's are stored, get each track from the album
       $.when.apply($, deferredAlbumId).done(() => {
         console.log('All album ID\'s retrieved');
-        console.log(myAlbumIdArray);
 
+        let deferredTrackId = getTracksPromises(myAlbumIdArray, myTracksArray);
+
+        // When all Track information is stored...
+        $.when.apply($, deferredTrackId).done(() => {
+          console.log('All track information retrieved');
+          console.log(myTracksArray);
+
+        }); // close $.when for getting Track information
       }); // close $.when for getting Album ID's
     }); // close $.when for getting Artist ID's
   }); // close $('artist').click event
@@ -48,17 +56,36 @@ let getArtistIdPromises = function(artistArray, compiledArray) {
 // 'artistIDArray' holds an array of artist ID's
 // 'compiledArray' will hold all returned Album ID's
 // method will return an array of AJAX calls
-let getAlbumsPromises = function(artistIDArray, compiledArray) {
-  console.log('getting album promises');
+let getAlbumsPromises = function(artistIdArray, compiledArray) {
   let deferreds = [];
 
-  for(let i = 0, j = artistIDArray.length; i < j; i++) {
+  for(let i = 0, j = artistIdArray.length; i < j; i++) {
     let newRequest = $.ajax({
-      url: 'albums/' + artistIDArray[i]
+      url: 'albums/' + artistIdArray[i]
     }).done((data) => {
       // data returns an array of Album objects
       for(let i = 0, j = data.length; i < j; i++) {
         compiledArray.push(data[i]['id']);
+      }
+
+    });
+
+    deferreds.push(newRequest);
+  }
+
+  return deferreds;
+}
+
+let getTracksPromises = function(albumIdArray, compiledArray) {
+  let deferreds = [];
+
+  for(let i = 0, j = albumIdArray.length; i < j; i++) {
+    let newRequest = $.ajax({
+      url: 'tracks/' + albumIdArray[i]
+    }).done((data) => {
+      // data returns an array of Track objects
+      for(let i = 0, j = data.length; i < j; i++) {
+        compiledArray.push(data[i]['title']);
       }
 
     });
