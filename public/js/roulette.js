57a0c06701591757d3071ca5f109713e20759860
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-04 15:02:50 -0500

    Add currentlyPlaying variable to controller

--- public/js/roulette.js
+++ public/js/roulette.js
@@ -4,12 +4,16 @@ let app = angular.module('Roulette', []);
 
 app.controller('RouletteController', function($http, $interval) {
 
+  this.currentlyPlaying = false;
+
   this.currentSong = {
     title:  '',
     artist: ''
   }
 
   this.playSong = function() {
+    this.currentlyPlaying = true;
+
     let myUrl = '/play'
 
     $http.get(myUrl)
@@ -19,6 +23,8 @@ app.controller('RouletteController', function($http, $interval) {
   };
 
   this.pauseSong = function() {
+    this.currentlyPlaying = !this.currentlyPlaying;
+
     let myUrl = '/pause'
 
     $http.get(myUrl)
@@ -29,6 +35,7 @@ app.controller('RouletteController', function($http, $interval) {
 
   // update current song information
   this.getSong = function() {
+    if(!this.currentlyPlaying) return false;
     let myUrl = '/currentSong';
 
     $http.get(myUrl)
@@ -41,6 +48,7 @@ app.controller('RouletteController', function($http, $interval) {
   };
 
   this.updateCurrentSong = function(title, artist) {
+
     this.currentSong['title']   = title;
     this.currentSong['artist']  = artist;
   };
