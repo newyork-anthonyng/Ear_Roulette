47b9c07160223fece772af482f49efdfa59a4c34
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-06 14:07:31 -0500

    Add default array into user schema

--- models/user.js
+++ models/user.js
@@ -4,11 +4,11 @@ let mongoose = require('mongoose');
 let Schema   = mongoose.Schema;
 
 let userSchema = new Schema({
-  name:     {type: String, required: true, unique: true},
-  password: {type: String, required: true},
+  name:     { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 
   // Array will be an array of Objects with key of "title" and "artist"
-  favorites: Array
+  favorites: { type: Array, default: [] }
 });
 
 let User = mongoose.model('User', userSchema);
