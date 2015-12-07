af389c1f126a702da11783cf22ee82afd3a77fb3
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-06 19:53:30 -0500

    Add bcrypt to salt and hash user passwords

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 'use strict'
 
-let mongoose = require('mongoose');
-let Schema   = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+const bcrypt   = require('bcrypt');
 
 let userSchema = new Schema({
   name:     { type: String, required: true, unique: true },
@@ -9,6 +10,25 @@ let userSchema = new Schema({
 
   // Array will be an array of Objects with key of "title" and "artist"
   favorites: { type: Array, default: [] }
+});
+
+userSchema.pre('save', (next) => {
+  let user = this;
+
+  // Has password if password has changed
+  if(!user.isModified('password')) return next();
+
+  // Salt the password
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if(err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 let User = mongoose.model('User', userSchema);
