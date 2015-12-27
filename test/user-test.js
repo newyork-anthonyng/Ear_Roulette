8de6a2969fdcf239fabd27d018b995b5d8bfc658
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-26 23:29:25 -0500

    Update test for /user/dislike to include username inside of request

--- test/user-test.js
+++ test/user-test.js
@@ -167,13 +167,17 @@ describe('User API', () => {
   it('should return all favorite songs on POST /user/dislike', (done) => {
     chai.request(server)
       .post('/user/dislike')
-      .send({ trackTitle: 'Mr. Brightside', trackArtist: 'The Killers' })
+      .send({
+        username:    'Hercules',
+        trackTitle:  'Mr. Brightside',
+        trackArtist: 'The Killers'
+      })
       .end((err, res) => {
         res.should.have.a.status(200);
         res.should.be.json;
         res.body.should.be.a('object');
         res.body.should.have.a.property('SUCCESS');
-        res.body.SUCCESS.should.be.false;
+        res.body.SUCCESS.should.be.true;
         res.body.should.have.a.property('tracks');
         res.body.tracks.should.be.a('array');
         res.body.tracks.length.should.be.eq(0);
