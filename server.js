b5bb582892e4d0c7ccf0ee6665b326ead2d6ff55
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-29 02:21:52 -0500

    Remove out commented code; unused code for authorization middleware

--- server.js
+++ server.js
@@ -39,32 +39,6 @@ app.get('/', (req, res) => {
   res.json({ SUCCESS: true });
 });
 
-// route middleware to verify token
-// app.use((req, res, next) => {
-//
-//   let token = req.headers['x-access-token'];
-//
-//   if(token) {
-//     jwt.verify(token, app.get('secret'), (err, decoded) => {
-//
-//       if(err) {
-//         return res.json({
-//             SUCCESS: false,
-//             MESSAGE: 'Failed to authenticate token'
-//         });
-//       } else {
-//         req.decoded = decoded;
-//         next();
-//       }
-//     });
-//   } else {
-//     return res.status(403).send({
-//       SUCCESS: false,
-//       MESSAGE: 'No token provided'
-//     });
-//   }
-// });
-
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Express server running...');
 });
