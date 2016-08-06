dddf6cb6db9ab6546670054cc711e0b4fdbeda09
Anthony Ng <newyork.anthonyng@gmail.com>
2016-08-06 07:56:24 -0400

    Create LikedSong component

---
+++ src/components/LikedSong.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const LikedSong = React.createClass({
+	createLink: function(title, artist) {
+		return 'youtube.com/' + title + '_' + 'artist';
+	},
+
+	render: function() {
+		const { title, artist, deleteSong } = this.props;
+
+		return (
+			<div className="liked-song">
+				<span className="title">
+					{title}
+				</span>
+				<span className="artist">
+					{artist}
+				</span>
+				<span className="link">
+					{this.createLink(title, artist)}
+				</span>
+				<button onClick={deleteSong}>
+					Delete
+				</button>
+			</div>
+		);
+	}
+});
+
+export { LikedSong };
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { LikedSong } from './components/LikedSong';
 
 const App = React.createClass({
 	render: function() {
+		const likedSongProps = {
+			title: 'Call Me Maybe',
+			artist: 'Carly Rae Jepsen',
+			link: 'youtube.com/call_me_maybe',
+			deleteSong: () => console.log('song deleted')
+		};
+
 		return (
 			<div>
 				Hello World
+				<LikedSong {...likedSongProps} />
 			</div>
 		);
 	}
