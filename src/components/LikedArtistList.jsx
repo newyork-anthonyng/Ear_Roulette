68c3b532d443f83f9586c52085146ee5282385f4
Anthony Ng <newyork.anthonyng@gmail.com>
2016-08-07 19:08:24 -0400

    Add LikedArtistList with passing tests

---
+++ src/components/LikedArtistList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { LikedArtist } from './LikedArtist';
+
+const LikedArtistList = React.createClass({
+	render: function() {
+		const { likedArtists, deleteArtist } = this.props;
+
+		return (
+			<div className="liked-artist-list">
+				<ul>
+					{likedArtists.map((likedArtist, index) => (
+						<li key={index}>
+							<LikedArtist
+								{...likedArtist}
+								deleteArtist={deleteArtist} />
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
+});
+
+export { LikedArtistList };
---
+++ test/components/LikedArtistList_spec.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+	renderIntoDocument,
+	scryRenderedComponentsWithType,
+	scryRenderedDOMComponentsWithTag,
+	Simulate
+} from 'react-addons-test-utils';
+import { LikedArtistList } from '../../src/components/LikedArtistList';
+import { LikedArtist } from '../../src/components/LikedArtist';
+import { expect } from 'chai';
+
+describe('LikedArtistList', () => {
+	it('renders a LikedArtist component for each artist', () => {
+		const likedArtists = [
+			{
+				artist: 'Justin Bieber'
+			},
+			{
+				artist: 'Metallica'
+			}
+		];
+		const component = renderIntoDocument(
+			<LikedArtistList
+				likedArtists={likedArtists}
+			/>
+		);
+		const likedArtistsEle = scryRenderedComponentsWithType(component, LikedArtist);
+		expect(likedArtistsEle.length).to.equal(2);
+	});
+
+	it('passes callback that is called with artist name', () => {
+		let deleteArtistInvoked = false;
+		const deleteArtist = (artist) => { deleteArtistInvoked = artist };
+		const likedArtists = [
+			{
+				artist: 'Justin Bieber'
+			},
+			{
+				artist: 'Metallica'
+			}
+		];
+		const component = renderIntoDocument(
+			<LikedArtistList
+				likedArtists={likedArtists}
+				deleteArtist={deleteArtist}
+			/>
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+		Simulate.click(buttons[0]);
+		expect(deleteArtistInvoked).to.equal('Justin Bieber');
+
+		Simulate.click(buttons[1]);
+		expect(deleteArtistInvoked).to.equal('Metallica');
+	});
+});
