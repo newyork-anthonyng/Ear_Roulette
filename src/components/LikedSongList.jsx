bb92bf021e78312bb316cb98053e4e68cccd100f
Anthony Ng <newyork.anthonyng@gmail.com>
2016-08-16 14:57:22 -0400

    Add button to toggle LikedSongList

--- src/components/LikedSongList.jsx
+++ src/components/LikedSongList.jsx
@@ -7,21 +7,43 @@ const LikedSongList = React.createClass({
 		deleteSong: React.PropTypes.func.isRequired
 	},
 
+	getInitialState: function() {
+		return {
+			isShowing: false
+		};
+	},
+
+	handleClick: function() {
+		this.setState({
+			isShowing: !this.state.isShowing
+		});
+	},
+
 	render: function() {
 		const { likedSongs, deleteSong } = this.props;
 
 		return (
-			<div className='liked-song-list'>
-				<ul>
-					{likedSongs.map((likedSong, index) => (
-						<li key={index}>
-							<LikedSong
-								{...likedSong}
-								deleteSong={deleteSong}
-							/>
-						</li>
-					))}
-				</ul>
+			<div>
+				<button
+					className='show-liked-songs'
+					onClick={this.handleClick}
+				>
+					Liked Songs
+				</button>
+				{this.state.isShowing ?
+					<div className='liked-song-list'>
+						<ul>
+							{likedSongs.map((likedSong, index) => (
+								<li key={index}>
+									<LikedSong
+										{...likedSong}
+										deleteSong={deleteSong}
+									/>
+								</li>
+							))}
+						</ul>
+					</div>
+				: null}
 			</div>
 		);
 	}
--- test/frontend/components/LikedSongList_spec.jsx
+++ test/frontend/components/LikedSongList_spec.jsx
@@ -3,6 +3,7 @@ import {
 	renderIntoDocument,
 	scryRenderedComponentsWithType,
 	findRenderedDOMComponentWithTag,
+	scryRenderedDOMComponentsWithTag,
 	Simulate
 } from 'react-addons-test-utils';
 import { LikedSongList } from '../../../src/components/LikedSongList';
@@ -10,23 +11,37 @@ import { LikedSong } from '../../../src/components/LikedSong';
 import { expect } from 'chai';
 
 describe('LikedSongList', () => {
-	it('renders a LikedSong component for each song', () => {
+	it('renders a button to show list of LikedSong components', () => {
 		const likedSongs = [
-			{
-				title: 'Baby',
-				artist: 'Justin Bieber'
-			},
-			{
-				title: 'Love Yourself',
-				artist: 'Justin Bieber'
-			}
+			{ title: 'Baby', artist: 'Justin Bieber' }
 		];
 		const component = renderIntoDocument(
 			<LikedSongList
 				likedSongs={likedSongs}
 			/>
 		);
-		const likedSongsEle = scryRenderedComponentsWithType(component, LikedSong);
+		const showListButton = findRenderedDOMComponentWithTag(component, 'button');
+		const buttonText = showListButton.textContent;
+
+		expect(buttonText).to.equal('Liked Songs');
+	});
+
+	it('renders a LikedSong component for each song', () => {
+		const likedSongs = [
+			{ title: 'Baby', artist: 'Justin Bieber' },
+			{ title: 'Love Yourself', artist: 'Justin Bieber' }
+		];
+		const component = renderIntoDocument(
+			<LikedSongList
+				likedSongs={likedSongs}
+			/>
+		);
+		let likedSongsEle = scryRenderedComponentsWithType(component, LikedSong);
+		expect(likedSongsEle.length).to.equal(0);
+
+		const showListButton = findRenderedDOMComponentWithTag(component, 'button');
+		Simulate.click(showListButton);
+		likedSongsEle = scryRenderedComponentsWithType(component, LikedSong);
 		expect(likedSongsEle.length).to.equal(2);
 	});
 
@@ -34,10 +49,7 @@ describe('LikedSongList', () => {
 		let deleteInvoked = false;
 		const deleteSong = ({ title, artist }) => (deleteInvoked = title + ' by ' + artist);
 		const likedSongs = [
-			{
-				title: 'Baby',
-				artist: 'Justin Bieber'
-			}
+			{ title: 'Baby', artist: 'Justin Bieber' }
 		];
 		const component = renderIntoDocument(
 			<LikedSongList
@@ -45,7 +57,9 @@ describe('LikedSongList', () => {
 				deleteSong={deleteSong}
 			/>
 		);
-		const deleteButton = findRenderedDOMComponentWithTag(component, 'button');
+		const showListButton = findRenderedDOMComponentWithTag(component, 'button');
+		Simulate.click(showListButton);
+		const deleteButton = scryRenderedDOMComponentsWithTag(component, 'button')[1];
 		Simulate.click(deleteButton);
 
 		expect(deleteInvoked).to.equal('Baby by Justin Bieber');
