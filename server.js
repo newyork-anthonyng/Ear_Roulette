007ce8cca71b42f35963d34996f3a1564b1e0e8f
Anthony Ng <newyork.anthonyng@gmail.com>
2016-08-18 11:25:00 -0400

    Update styling for album art

--- server.js
+++ server.js
@@ -21,16 +21,10 @@ app.get('/get_songs', (req, res) => {
 				preview: 'https://p.scdn.co/mp3-preview/4d80119d75592741b76faf04fe294b2e02cb88ee'
 			},
 			{
-				title: 'Guns and Ships',
-				artist: 'Original Broadway Cast of Hamilton',
-				image: 'https://i.scdn.co/image/cab99e12568f975eacaf11dac9493d7c07e51e4e',
-				preview: 'https://p.scdn.co/mp3-preview/06062a76220ae6d7df233663fe24d49d45d6dee0'
-			},
-			{
-				title: 'My Shot',
-				artist: 'Original Broadway Cast of Hamilton',
-				image: 'https://i.scdn.co/image/cab99e12568f975eacaf11dac9493d7c07e51e4e',
-				preview: 'https://p.scdn.co/mp3-preview/4d80119d75592741b76faf04fe294b2e02cb88ee'
+				title: 'Dancing Queen',
+				artist: 'Abba',
+				image: 'https://i.scdn.co/image/6acb2f6b68a0fa577d9afed7547a2e6d9a48e08a',
+				preview: 'https://p.scdn.co/mp3-preview/07dbfe8eb7729d99a182b26fa1ec2f3467ba86f5'
 			},
 			{
 				title: 'Guns and Ships',
@@ -38,24 +32,6 @@ app.get('/get_songs', (req, res) => {
 				image: 'https://i.scdn.co/image/cab99e12568f975eacaf11dac9493d7c07e51e4e',
 				preview: 'https://p.scdn.co/mp3-preview/06062a76220ae6d7df233663fe24d49d45d6dee0'
 			},
-			{
-				title: 'My Shot',
-				artist: 'Original Broadway Cast of Hamilton',
-				image: 'https://i.scdn.co/image/cab99e12568f975eacaf11dac9493d7c07e51e4e',
-				preview: 'https://p.scdn.co/mp3-preview/4d80119d75592741b76faf04fe294b2e02cb88ee'
-			},
-			{
-				title: 'Guns and Ships',
-				artist: 'Original Broadway Cast of Hamilton',
-				image: 'https://i.scdn.co/image/cab99e12568f975eacaf11dac9493d7c07e51e4e',
-				preview: 'https://p.scdn.co/mp3-preview/06062a76220ae6d7df233663fe24d49d45d6dee0'
-			},
-			{
-				title: 'My Shot',
-				artist: 'Original Broadway Cast of Hamilton',
-				image: 'https://i.scdn.co/image/cab99e12568f975eacaf11dac9493d7c07e51e4e',
-				preview: 'https://p.scdn.co/mp3-preview/4d80119d75592741b76faf04fe294b2e02cb88ee'
-			}
 		]
 	});
 });
